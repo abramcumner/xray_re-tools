d60c26717933ae8b32280651118576c593b2e8bc
RedPython_cp <SND\RedPython_cp>
2011-05-16 18:22:34 +0000

    Fixed minor shit in previous commit for vs2008 (x64). Removed 3ds Max and Maya SDK because of lack of necessity in them at this time.

--- trunk/sources/3rd party/3dmax_sdk/8.0/include/maxscrpt/phymcpro.h
+++
@@ -1,5 +0,0 @@
-	// Protocols for Physique Mod Context Vertex Interface classes
-	
-	def_visible_generic_debug_ok	(ConvertToRigid,			"ConvertToRigid");
-	def_visible_generic_debug_ok	(AllowBlending,				"AllowBlending");
-	def_visible_generic_debug_ok	(GetVertexInterface,		"GetVertexInterface");
--- trunk/sources/3rd party/3dmax_sdk/8.0/include/maxscrpt/phyrgpro.h
+++
@@ -1,5 +0,0 @@
-	// Protocols for Physique Rigid Vertex Interface classes
-
-	def_visible_generic_debug_ok	( GetNode,			"GetNode");
-	def_visible_generic_debug_ok	( GetOffsetVector,	"GetOffsetVector");
-
